9b1e5263f74366b1d7840d5387bb52104fa4a993
Gustavo Cabreira <gustavo.softdev@gmail.com>
2023-04-03 04:14:40 -0300

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-3 mt-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-3 mt-6">
+        <p class="text-white font-medium text-xl">Create new user</p>
+    </div>
+
+    <div class="">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -14,27 +18,34 @@
                         <input type="text" name="name" placeholder="Name"/>
                         <input type="text" name="email" placeholder="Email"/>
                         <input type="text" name="password" placeholder="Password"/>
-                        <button type="submit">Create</button>
+                        <button class="bg-blue-600 px-2 py-2 text-white uppercase ml-2" type="submit">Create
+                        </button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <ul>
-                        @foreach($users as $user)
-                            <li>
-                                {{$user->name}} -
-                                <a href="{{route('users.show', ['user' => $user])}}">update</a>
-                                <a href="{{route('users.delete', ['user' => $user])}}">delete</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6 mb-3">
+        <p class="text-white font-medium text-xl">Users list</p>
+    </div>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+                <ul>
+                    @foreach($users as $user)
+                        <li class="flex items-center justify-between">
+                            {{$user->name}}
+                            <div>
+                                <a class="bg-orange-500 px-2 py-2 text-white uppercase ml-2"
+                                   href="{{route('users.show', ['user' => $user])}}">update</a>
+                                <a class="bg-red-500 px-2 py-2 text-white uppercase ml-2"
+                                   href="{{route('users.delete', ['user' => $user])}}">delete</a>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,19 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Update User') }}
         </h2>
     </x-slot>
 
-    <div class="py-3 mt-6">
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
+        <p class="text-white font-medium text-xl">Update user</p>
+    </div>
+
+    <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="{{route('users.update', ['user' => $user])}}" method="post">
+                    <form action="{{route('users.update', ['user' => $user])}}" method="post"
+                          class="flex items-center justify-between">
                         @csrf
-                        <input type="text" name="name" placeholder="Name" value="{{$user->name}}"/>
-                        <input type="text" name="email" placeholder="Email" value="{{$user->email}}"/>
-                        <button type="submit">Update</button>
+                        <div>
+                            <input class="text-black" type="text" name="name" placeholder="Name"
+                                   value="{{$user->name}}"/>
+                            <input class="text-black" type="text" name="email" placeholder="Email"
+                                   value="{{$user->email}}"/>
+                            <button class="bg-blue-600 px-2 py-2 text-white uppercase ml-2" type="submit">Update
+                            </button>
+                        </div>
+                        <a class="bg-white px-2 py-2 text-black uppercase ml-2" href="{{route('dashboard')}}">Go
+                            back</a>
                     </form>
                 </div>
             </div>
