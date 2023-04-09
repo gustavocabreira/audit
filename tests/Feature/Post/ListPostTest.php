0e0d7fa3c1005688a1051703d27635ded2d6a844
Gustavo Cabreira <gustavo.softdev@gmail.com>
2023-04-09 20:48:15 -0300

    should list all posts

--- tests/Feature/Post/ListPostTest.php
+++ tests/Feature/Post/ListPostTest.php
@@ -1,9 +1,27 @@
 <?php
 
+use App\Http\Livewire\Post\ListPost;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
 
 uses(RefreshDatabase::class);
 
-todo('should list all posts');
+it('should list all posts', function () {
+    // Arrange
+    $user = User::factory()->create();
+    $post = Post::factory()->create();
+    actingAs($user);
+
+    // Act
+    $lw = livewire(ListPost::class);
+
+    // Assert
+    $lw->assertSee($post->title)
+        ->assertSee($post->author->name);
+});
+
 todo('user can only update posts that belongs to him');
 todo('user can only delete posts that belongs to him');
