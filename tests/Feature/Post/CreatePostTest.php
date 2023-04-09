530fb86a15caf014b363dcbf455308c0a9eccd97
Gustavo Cabreira <gustavo.softdev@gmail.com>
2023-04-09 20:18:56 -0300

    should emmit a createdPost event

--- tests/Feature/Post/CreatePostTest.php
+++ tests/Feature/Post/CreatePostTest.php
@@ -9,6 +9,7 @@ use function Pest\Livewire\livewire;
 
 uses(RefreshDatabase::class);
 
+
 it('should be able to create a new post', function () {
     // Arrange
     $user = User::factory()->create();
@@ -49,6 +50,21 @@ test('body and title can not be empty', function () {
     $lw->assertHasErrors();
 });
 
-todo('should emmit a createdPost event');
+it('should emmit a createdPost event', function () {
+    // Arrange
+    $user = User::factory()->create();
+    actingAs($user);
+
+    // Act
+    $lw = livewire(CreatePost::class)
+        ->set('title', 'Title')
+        ->set('body', 'Body')
+        ->set('publish_date', now()->format('Y-m-d'))
+        ->call('create');
+
+    // Assert
+    $lw->assertEmitted('createdPost');
+});
+
 todo('should send an email to the user that has created the post');
 todo('should clear all fields after creating');
