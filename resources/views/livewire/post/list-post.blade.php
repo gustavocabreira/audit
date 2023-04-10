928554b7dba57639e02de53212ea09835a2d73d9
Gustavo Cabreira <gustavo.softdev@gmail.com>
2023-04-09 21:16:58 -0300

    wip

--- resources/views/livewire/post/list-post.blade.php
+++ resources/views/livewire/post/list-post.blade.php
@@ -25,18 +25,24 @@
             <p class="text-white font-medium text-xl">Scheduled posts</p>
         </div>
         @foreach($scheduledPosts as $post)
-            <article class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-5 p-5">
+            <article
+                class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-5 p-5 flex align-items-center justify-between">
                 <div>
                     <h1>{{ucfirst($post->title)}}</h1>
                     <h6>
                         Created by <span class="text-green-300 font-semibold">{{$post->author->name}}</span>
-                        and will be published at <span>{{$post->published_at->format('Y/m/d')}}</span>
+                        and published at <span>{{$post->published_at->format('Y/m/d')}}</span>
                     </h6>
                 </div>
 
-                @can('update', $post)
-                    <button type="button">Update</button>
-                @endcan
+                <div>
+                    @can('update', $post)
+                        <button type="button" class="bg-orange-500 px-2 py-2 text-white uppercase ml-2">Update</button>
+                    @endcan
+                    @can('delete', $post)
+                        <button type="button" class="bg-red-500 px-2 py-2 text-white uppercase ml-2">Delete</button>
+                    @endcan
+                </div>
             </article>
         @endforeach
     @endif
